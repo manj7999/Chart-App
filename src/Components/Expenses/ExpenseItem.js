7b8b298923d723e62e9a956d03d6d78f1b31cf72
Ankit Manjhi <ankitmanjhi286@gmail.com>
2024-01-08 03:02:13 +0530

    Your commit message here

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -9,7 +9,7 @@ const ExpenseItem = (props) => {
   console.log("ExpenseItem evaluated by React");
 
   const clickHandler = () => {
-    setTitle['Updated!'];
+    setTitle('Updated!');
     console.log(title);
   };
 
@@ -17,7 +17,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item_description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item_price">${props.amount}</div>
       </div>
       {/* <button onClick={clickHandler}>Change title</button> */}
